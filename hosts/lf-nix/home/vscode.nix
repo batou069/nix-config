7b5898a1ccfa730f4c56f3d7914bbf333e8de3d3
installer <installer@gmail.com>
2025-06-28 01:15:53 +0300

    asd

--- hosts/lf-nix/home/vscode.nix
+++ hosts/lf-nix/home/vscode.nix
@@ -86,7 +86,13 @@
         mishkinf.goto-next-previous-member
         catppuccin.catppuccin-vsc
         njpwerner.autodocstring
-      ];
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "remote-ssh-edit";
+        publisher = "ms-vscode-remote";
+        version = "0.47.2";
+        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+      ;
     };
   };
 }
