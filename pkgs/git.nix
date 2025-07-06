200ddedf138885ab463895d59438098de3f6749f
installer <installer@gmail.com>
2025-07-06 10:16:48 +0300

    git aliases

--- pkgs/git.nix
+++ pkgs/git.nix
@@ -6,6 +6,7 @@
     enable = true;
     userName = "Laurent Flaster";
     userEmail = "laurentf84@gmail.com";
+    # hooks = {pre-commit = ./pre-commit-script};
     extraConfig = {
       # Configure Meld as the default diff tool
       diff = {
@@ -20,6 +21,26 @@
       alias = {
         diffpick = "!sh -c 'git diff --name-only | fpp --no-file-checks | xargs -r git difftool'";
       };
+      aliases = {
+          ga = "git add";
+          g = "git";
+          gp = "git push -u origin";
+          gpl = "git pull";
+          gdc = "git diff --cached";
+          gd = "git diff";
+          gsh = "git show";
+          gl = "git log";
+          ggraph = "git log --oneline --decorate --graph --all";
+          gcom = "git commit -m";
+          gst = "git status";
+          gb ="git branch";
+          gco = "git checkout";
+          gcb = "git checkout -b";
+          gbm = "git merge";
+          gbd = "git branch -d";
+          gbdD = "git branch -D";
+          gu = "git reset HEAD --mixed";
+        };
     };
   };
 }
