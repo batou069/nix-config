049b5fac432281253790666e0c54765992a87bc0
installer <installer@gmail.com>
2025-11-27 15:50:37 +0200

    lets fix the sops error

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,10 +1,9 @@
+# modules/sops.nix
 { ... }: {
   sops = {
     defaultSopsFile = ../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "/home/lf/nix/secrets/age-key.txt";
-
-    # This block is for the NixOS module to know what's in the file.
     secrets = {
       "github_pat" = { };
       "api_keys/openai" = { };
@@ -17,20 +16,5 @@
       "influxdb" = { };
       "ssh_keys/github" = { };
     };
-  };
-
-  # This is the CRITICAL part for the standalone Home Manager module.
-  # Each secret must be declared individually here.
-  sops.secrets = {
-    "github_pat" = { };
-    "api_keys/openai" = { };
-    "api_keys/anthropic" = { };
-    "api_keys/gemini" = { };
-    "bitwarden" = { };
-    "api_keys/tavily" = { };
-    "api_keys/brave_search" = { };
-    "api_keys/github_mcp" = { };
-    "influxdb" = { };
-    "ssh_keys/github" = { };
   };
 }
