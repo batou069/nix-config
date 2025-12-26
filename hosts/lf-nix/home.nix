106738412fe822db40b9dcd8858d98c61e9147d5
installer <installer@gmail.com>
2025-12-26 02:51:09 +0200

    fix(home): resolve infinite recursion by moving system module import

--- hosts/lf-nix/home.nix
+++ hosts/lf-nix/home.nix
@@ -1,9 +1,10 @@
 # This is a NixOS module that configures the home-manager service for a specific user.
 { username, ... }: {
+  imports = [ ../default/home.nix ];
+
   home-manager.users.${username} = {
     # This is where we import all the modules for this user's Home Manager configuration.
     imports = [
-      ../default/home.nix
       ../../home/home-manager.nix
       ../../home/systemd.nix
       ../../home/xdg.nix
